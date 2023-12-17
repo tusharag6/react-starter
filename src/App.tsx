148f1542172f0393720bf32a173b8485b3bd29d2
Tushar Agrawal <tushar.code.inbox@gmail.com>
2023-12-17 12:42:51 +0530

    setup testing with vitest, testing library and jsdom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
-function App() {
-  return <>Hello World</>;
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
 }
 
-export default App;
+export function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
